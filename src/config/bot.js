3b27fd74f1b99f86eb869983d10d098cbe6937b6
alexalbo35480-hub <alexalbo35480@gmail.com>
2026-07-03 04:58:08 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Yusuf Ta Gueule",
         // Activity type number (0 = Playing).
         type: 0,
       },
